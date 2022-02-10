1c1d8a8fd4c521f3d52aa5e4c4f4d0c1b4de2bfd
Ruslan <kornerus@fjfi.cvut.cz>
2022-02-10 11:14:33 +0300

    create `DialogScraper` and scrape categories

--- src/dialog/scraper.py
+++ src/dialog/scraper.py
@@ -1,8 +1,36 @@
+import time
+from typing import List, Optional
+
 from bs4 import BeautifulSoup as BS
 import requests as req
 
 
-BASE_URL = "https://dialog.ru/catalog/lekarstva_i_bady/"
+class DialogScraper:
+    def __init__(self, sleeping: int = 5) -> None:
+        self.BASE_URL = "https://dialog.ru/catalog/lekarstva_i_bady/"
+        self.sleeping = sleeping
 
-response = req.get(BASE_URL)
-soup = BS(response.text, 'lxml')
+    def start_scrape(self) -> None:
+        """ Start scrape dialog """
+        soup = self.get_soup()
+        self.get_categories(soup)
+
+
+    def get_soup(self, url: Optional[str] = None) -> BS:
+        """ send request to url and get html - soup object """
+        time.sleep(self.sleeping)
+        if not url:
+            url = self.BASE_URL
+        response = req.get(url)
+        return BS(response.text, 'lxml')
+
+    def get_categories(self, soup: BS) -> List[dict]:
+        """ get all categories with data """
+        categories = []
+        ul = soup.find('ul', class_="nM-n")
+        for li in ul.find_all('li', class_="yB03"):
+            title = li.text.strip()
+            path = li.find('a')['href']
+            categories.append({"title": title, "path": path})
+
+        return categories
